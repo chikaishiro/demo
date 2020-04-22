bd7f85b5a6766e41980716150c03bdf6946f1077
chikaishiro <gran_turismo@126.com>
2020-04-22 16:31:13 +0800

    Update cut_posseg.py

--- cut_posseg.py
+++ cut_posseg.py
@@ -13,18 +13,14 @@ def get_tag_from_sentence(sentence):
         tag = ""
     return tag
     
-def stopwordslist(stopwords_file):
-    stopwords = [line.strip() for line in open(stopwords_file,encoding='UTF-8').readlines()]
-    return stopwords
 
 def cut_extract(sentence):
-    stopwords = stopwordslist("stopwords.txt")
     tag = get_tag_from_sentence(sentence)
     sentence = re.sub(r'[^\u4e00-\u9fa5]+', '', sentence)
     sentence_depart = jieba.posseg.cut(sentence.strip())
     outstr = ""
     for word in sentence_depart:
-        if word.flag not in ['d','p','t','x','r','a','b','m','c','vg','u','nj'] and len(word.word) > 1 and word not in stopwords:
+        if word.flag not in ['d','p','t','x','r','a','b','m','c','vg','u','nj'] and len(word.word) > 1 :
             outstr += word.word
             outstr += " "
     return outstr + ";" + tag
